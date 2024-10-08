7df5521e0ea682aefba15a6b0799ccd838f48780
Andreas Maucher <Andreas-maucher@gmx.de>
2024-10-08 17:14:20 +0200

    update

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 
-int main(int ac, char **av)
-{
-    if (ac < 1 || ac > 2)
-    {
-        std::cout << "Invalid number of arguments" << std::endl;
-        return (1);
-    }
+int main(int ac, char **av) {
+  if (ac < 1 || ac > 2) {
+    std::cout << "Invalid number of arguments" << std::endl;
+    return (1);
+  }
 }
