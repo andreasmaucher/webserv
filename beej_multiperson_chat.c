d28539f642bd1b05e2e0cb94fb739ec45f0bc229
Michael Rizhakov <mrizakov@student.42berlin.de>
2024-10-08 22:09:38 +0200

    Mulitichat working

--- beej_multiperson_chat.c
+++ beej_multiperson_chat.c
@@ -6,7 +6,7 @@
 /*   By: mrizhakov <mrizhakov@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 14:17:32 by mrizakov          #+#    #+#             */
-/*   Updated: 2024/10/07 19:36:03 by mrizhakov        ###   ########.fr       */
+/*   Updated: 2024/10/08 22:09:06 by mrizhakov        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@
 #include <netdb.h>
 #include <poll.h>
 
-#define PORT "9034"
+#define PORT "9036"
+
+
 
 void *get_in_addr(struct sockaddr *sa) {
     if (sa->sa_family == AF_INET) {  // Check if using IPv4
@@ -119,5 +121,121 @@ void del_from_pfds(struct pollfd pfds[], int i, int *fd_count)
     pfds[i] =  pfds[*fd_count - 1]; // replace the fd to be removed (fd[i]) with the last element of array
     (*fd_count)--; // make the size of the array smaller by one
     // Not doing realloc() to change the actual size of the array, since this can be slow
+}
+
+int main(void)
+{
+    int listener_fd; //Listening socket fd
+    int new_fd; // Newly accepted fd
+    struct sockaddr_storage remoteaddr; // Client address (both IPv4 and IPv6)
+    socklen_t addrlen;
+    char buf[256]; //Buff for client data
+    char remoteIP[INET6_ADDRSTRLEN]; //To store IP address in string form
+    int fd_count = 0; // Current number of used fds
+    int fd_size = 5; // Initial size of struct to hold all fds
+    struct pollfd *pfds = malloc(sizeof *pfds * fd_size);
+
+    listener_fd = get_listener_socket();
+    if (listener_fd == -1) {
+        fprintf(stderr, "error getting listening socket\n");
+        exit(1);
+    }
+    pfds[0].fd = listener_fd;
+    pfds[0].events = POLLIN; //Watch for incoming data on the listener
+    fd_count = 1; // There is only 1 listener on the socket
+
+
+    // Manually added option to allow to reuse ports straight after closing the server - SO_REUSEADDR
+    // If this option is not configured, ports will be in TIME_WAIT state even after they are closed and can be used only after they timeout
+    int opt = 1;
+    if (setsockopt(listener_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        perror("setsockopt(SO_REUSEADDR) failed");
+        exit(1);
+    }
+    
+    // Main loop
+     while (1)
+     {
+        int poll_count = poll(pfds, fd_count, -1);
+        if (poll_count == -1) {
+            perror("poll");
+            exit(1);
+        }
+        
+        // Run through the existing connections looking for data to read
+        for (int i = 0; i < fd_count; i++)
+        {
+            // Check if an fd is ready to read
+            if (pfds[i].revents & POLLIN) { // Received a connection
+                if (pfds[i].fd == listener_fd) { // If listener is ready to read, handle new connection
+                    addrlen = sizeof remoteaddr;
+                    new_fd = accept(listener_fd, 
+                        (struct sockaddr *)&remoteaddr, 
+                        &addrlen);
+                    if (new_fd == -1) {
+                        perror("accept");
+                    } else {
+                        add_to_pfds(&pfds, new_fd, &fd_count, &fd_size);
+                        
+                        printf("pollserver: new connection from %s on socket %d\n",
+                            inet_ntop(remoteaddr.ss_family,
+                                get_in_addr((struct sockaddr*)&remoteaddr),
+                                remoteIP, INET6_ADDRSTRLEN),
+                            new_fd);
+                    }
+                } else {
+                    // If not the listener, we're just the regular client
+                    int nbytes = recv(pfds[i].fd, buf, sizeof buf, 0);
+                    int sender_fd = pfds[i].fd;
+                    
+                    if (nbytes <= 0) {
+                        // Got error or connection closed by client
+                        if (nbytes == 0) {
+                            // Connection closed
+                            printf("pollserver: socket %d hung up \n", sender_fd);
+                        } else {
+                            perror("recv");
+                        }
+                        close(pfds[i].fd); //Closing fd
+
+                        del_from_pfds(pfds, i, &fd_count);
+
+                    } else {
+                        // Received some good data from the client
+                        // Printing out received data
+                        buf[nbytes] = '\0';
+                        printf("Received: %s\n", buf);
+
+                        // sending a msg "close" from the client closes the connection
+
+                        if (strncmp(buf, "close", 5) == 0) 
+                        {
+                            printf("Received: %s\n", buf); // Print the message.
+                            printf("Closing connection\n");   // Print the message.
+                            close(new_fd);
+                            close(listener_fd);
+                            // freeaddrinfo(res); // free the linked list
+                            return 0;
+                        }
+                        for (int j = 0; j < fd_count; j++) {
+                            
+                            // Send to everyone!
+                            int dest_fd = pfds[j].fd;
+                            // Except the listener and ourselves
+                            if (dest_fd != listener_fd && dest_fd != sender_fd) {
+                                if (send(dest_fd, buf, nbytes, 0) == -1) {
+                                    perror("send");
+                                }
+                            }
+                        }
+                    }
+                    
+                } // END handle data from client    
+            } // END got ready-to-read from poll()
+        } // END looping through file descriptors
+     } // END  while(1) loop 
+
+
+    return 0;
 }
 
