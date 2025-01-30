2da6ea1f7502e2ec4fce5f4a575537fb63e50591
Michael Rizhakov <mrizakov@student.42berlin.de>
2025-01-31 00:34:36 +0100

    Improved efficiency, response time and availability in siege

--- include/debug.hpp
+++ include/debug.hpp
@@ -3,7 +3,7 @@
 
 #include <iostream>
 
-#define DEBUG ON
+// #define DEBUG ON
 
 #ifdef DEBUG
 #define DEBUG_MSG(msg, var) std::cerr << "[DEBUG] " << msg << ": " << var << std::endl
@@ -11,7 +11,7 @@
 #define DEBUG_MSG(msg, var) ((void)0)
 #endif
 
-//#define DEBUG_1 ON
+#define DEBUG_1 ON
 
 #ifdef DEBUG_1
 #define DEBUG_MSG_1(msg, var) std::cerr << "[DEBUG] " << msg << ": " << var << std::endl
